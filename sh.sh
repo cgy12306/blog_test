7924153217f1f9e1450368eb7834449acb4e75be
cgy12306 <cgy12306@naver.com>
2020-12-14 17:39:11 +0900

    test

---
+++ sh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+hugo -t compose
+
+cd public
+git pull origin master
+git add .
+git commit -m "test"
+git push origin master
+cd ..
+git add .
+git commit -m "test"
+git push origin master
+
+
